8f140739fce559fb309609a6b82b10bc1db61b7b
Anastasija Cvetkovic <anastasija.cvetkovic1@outlook.com>
2024-12-21 18:55:35 +0100

    feat(core): add a blank solution and a class library for logic

---
+++ LEDDE/LEDDE.Library/LED/LEDMatrix.cs
@@ -0,0 +1,46 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+namespace LEDDE.Library.LED
+{
+    public class LEDMatrix
+    {
+        private readonly LEDPixel[] _matrix;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public LEDMatrix(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            _matrix = new LEDPixel[Width * Height];
+
+            //InitializePixels();
+        }
+
+        private void InitializePixels()
+        {
+            for (int i = 0; i < _matrix.Length; i++)
+            {
+                _matrix[i] = new LEDPixel();
+            }
+        }
+        private int GetIndex(int x, int y)
+        {
+            return y * Width + x;
+        }
+        public void SetPixelColor(int x, int y, Rgba32 color)
+        {
+            //x - horizontal, y - vertical
+            if (x < 0 || x >= Width)
+            {
+                throw new ArgumentOutOfRangeException(nameof(x), "The x coordinate is out of bounds.");
+            }
+            if (y < 0 || y >= Height)
+            {
+                throw new ArgumentOutOfRangeException(nameof(y), "The y coordinate is out of bounds.");
+            }
+            int index = GetIndex(x, y);
+            _matrix[index].SetColor(color);
+        }
+
+    }
+}
---
+++ LEDDE/LEDDE.Library/LED/LEDPixel.cs
@@ -0,0 +1,19 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+namespace LEDDE.Library.LED
+{
+    internal class LEDPixel
+    {
+        public Rgba32 Color { get; set; }
+        public LEDPixel() : this(new Rgba32(0, 0, 0, 0)) { }
+        public LEDPixel(Rgba32 color)
+        {
+            this.Color = color;
+        }
+        public void SetColor(Rgba32 color) 
+        {
+            Color = color;
+        }
+
+    }
+}
